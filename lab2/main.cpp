cfb1b5cb29a19bff1fc5daf29a705b0756c69802
Егор <malchihin2004@gmail.com>
2025-10-08 23:30:25 +0300

    all

---
+++ lab2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    vector<string> paths;
+    bool show_all = false;
+    bool long_format = false;
+    
+    // Здесь будет разбор аргументов
+    
+    cout << "myls started" << endl;
+    
+    return 0;
+}
